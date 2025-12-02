063bd6cf95c6df7cdf4af0a7a9a1937de6299aa1
Jake Abed <jakeabed@proton.me>
2025-12-01 22:31:18 -0800

    Complete day two

---
+++ ts/day2.ts
@@ -0,0 +1,76 @@
+const rawInput = await Deno.readTextFile("./inputs/day2.txt");
+const idRanges = rawInput.split(",").map((a) => a.trim());
+
+const parseIdRange = (idRange: string): [string, string] => {
+  return idRange.split("-") as [string, string];
+};
+
+export const solvePartOne = () => {
+  const badIds: number[] = [];
+
+  for (const idRange of idRanges) {
+    const [start, end] = parseIdRange(idRange);
+    let cur = parseInt(start) as number;
+
+    while (cur <= parseInt(end)) {
+      const current = cur.toString();
+
+      if (current[0] === "0") {
+        cur++;
+        continue;
+      }
+
+      if (current.length % 2 != 0) {
+        cur++;
+        continue;
+      }
+
+      const firstHalf = current.slice(0, current.length / 2);
+      const secondHalf = current.slice(current.length / 2);
+      if (firstHalf === secondHalf) {
+        badIds.push(cur);
+      }
+      cur++;
+    }
+  }
+
+  console.log(badIds.reduce((id, acc) => id + acc, 0));
+};
+
+export const solvePartTwo = () => {
+  const badIds: number[] = [];
+
+  for (const idRange of idRanges) {
+    const [start, end] = parseIdRange(idRange);
+    let cur = parseInt(start) as number;
+
+    while (cur <= parseInt(end)) {
+      const current = cur.toString();
+
+      if (current[0] === "0") {
+        cur++;
+        continue;
+      }
+
+      const l = current.length;
+
+      const firstHalf = current.slice(0, current.length / 2);
+      const secondHalf = current.slice(current.length / 2);
+      if (firstHalf === secondHalf) {
+        badIds.push(cur);
+      } else {
+        for (let i = 1; i < l / 2; i++) {
+          const substring = current.slice(0, i);
+          if (substring.repeat(l / substring.length) === current) {
+            badIds.push(cur);
+            break;
+          }
+        }
+      }
+
+      cur++;
+    }
+  }
+
+  console.log(badIds.reduce((id, acc) => id + acc, 0));
+};
--- ts/main.ts
+++ ts/main.ts
@@ -1,4 +1,8 @@
 import * as DayOne from './day1.ts';
+import * as DayTwo from './day2.ts';
 
 DayOne.solvePartOne();
 DayOne.solvePartTwo();
+
+DayTwo.solvePartOne();
+DayTwo.solvePartTwo();
