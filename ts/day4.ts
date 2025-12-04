6193895c2c434a2b820f1831e059e005a660c2f4
Jake Abed <jakeabed@proton.me>
2025-12-03 22:07:43 -0800

    Finish day4 part two, ez

--- ts/day4.ts
+++ ts/day4.ts
@@ -37,4 +37,45 @@ export const solvePartOne = () => {
   console.log(reachable);
 };
 
-solvePartOne();
+export const solvePartTwo = (reachable: number = 0) => {
+  const numberOfRolls = paperMatrix.flat().filter((c) => c === PAPER).length;
+  const width = paperMatrix[0].length;
+  const height = paperMatrix.length;
+  const edgesX = [0, width - 1];
+  const edgesY = [0, height - 1];
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < height; x++) {
+      const spaces = [
+        x === edgesX[0] || y === edgesY[0] ? SPACE : paperMatrix[y - 1][x - 1], // nw
+        x === edgesX[1] || y === edgesY[0] ? SPACE : paperMatrix[y - 1][x + 1], // ne
+        x === edgesX[1] || y === edgesY[1] ? SPACE : paperMatrix[y + 1][x + 1], // se
+        x === edgesX[0] || y === edgesY[1] ? SPACE : paperMatrix[y + 1][x - 1], // sw
+        x === edgesX[0] ? SPACE : paperMatrix[y][x - 1], // w
+        x === edgesX[1] ? SPACE : paperMatrix[y][x + 1], // e
+        y === edgesY[0] ? SPACE : paperMatrix[y - 1][x], // n
+        y === edgesY[1] ? SPACE : paperMatrix[y + 1][x], // s
+      ];
+
+      const openSpots = spaces.filter((s) => s === SPACE).map((_) => 1).reduce(
+        (a, acc) => a + acc,
+        0,
+      );
+
+      if (openSpots > 4 && paperMatrix[y][x] === PAPER) {
+        reachable++;
+        paperMatrix[y][x] = SPACE;
+      }
+    }
+  }
+
+  const newNumberOfRolls = paperMatrix.flat().filter((c) => c === PAPER).length;
+
+  if (newNumberOfRolls < numberOfRolls) {
+    solvePartTwo(reachable);
+  } else {
+    console.log(reachable);
+  }
+};
+
+solvePartTwo();
--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,7 @@
 import * as DayOne from './day1.ts';
 import * as DayTwo from './day2.ts';
 import * as DayThree from './day3.ts';
+import * as DayFour from './day4.ts';
 
 DayOne.solvePartOne();
 DayOne.solvePartTwo();
@@ -9,3 +10,7 @@ DayTwo.solvePartOne();
 DayTwo.solvePartTwo();
 
 DayThree.solvePartOne();
+DayThree.solvePartTwo();
+
+DayFour.solvePartOne();
+DayFour.solvePartTwo();
