bb34eb37da272b442e1b3336c38e14588fac9ab2
Jake Abed <jakeabed@proton.me>
2025-12-07 13:29:00 -0800

    failed day seven, part two :(

--- ts/day7.ts
+++ ts/day7.ts
@@ -1,6 +1,8 @@
 const rawInput = await Deno.readTextFile("./inputs/day7.txt");
 const rows = rawInput.split("\n").map((a) => a.trim()).filter((a) => a !== "");
 
+const partTwoRows = rows.map((row) => row.split("").filter((c) => c !== ""));
+
 export const solvePartOne = (): { count: number; time: number } => {
   const tZero = performance.now();
   let count = 0;
@@ -41,4 +43,43 @@ export const solvePartOne = (): { count: number; time: number } => {
   return { count, time };
 };
 
-console.log(solvePartOne());
+type MemoKey = string;
+type Memo = Record<string, number>;
+
+export const solvePartTwo = (): { count: number; time: number } => {
+  const tZero = performance.now();
+  const startPosition = rows[0].indexOf("S");
+  const memo: Memo = {};
+
+  const count = getTimelines(startPosition, 0, partTwoRows, memo);
+  const time = performance.now() - tZero;
+  return { count, time };
+};
+
+const getTimelines = (
+  pos: number,
+  idx: number,
+  grid: string[][],
+  memo: Memo,
+) => {
+  const key = `${pos}/${idx}` as MemoKey;
+
+  if (key in memo) {
+    return memo[key]!;
+  }
+
+  let total = 0;
+  for (let i = idx; i < grid.length; i++) {
+    if (grid[i][pos] === "^") {
+      total = 0;
+      total += getTimelines(pos + 1, i + 1, grid, memo);
+      total += getTimelines(pos - 1, i + 1, grid, memo);
+      return total;
+    }
+  }
+
+  memo[key] = 1;
+  return 1;
+};
+
+console.log(solvePartTwo());
